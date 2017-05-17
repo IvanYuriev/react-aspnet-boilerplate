1bec8c095550ba6f921b04e1344de1cc76369f87
Ivan Yuriev <Ivan.Yuriev@avionero.com>
2017-05-17 14:11:11 +0600

    AddUserSecrets method is obsolete use generic method instead AddUserSecrets<>

--- src/ReactBoilerplate/Startup.cs
+++ src/ReactBoilerplate/Startup.cs
@@ -31,7 +31,7 @@ namespace ReactBoilerplate
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                builder.AddUserSecrets<Startup>();
             }
 
             EnvironmentVariablesExtensions.AddEnvironmentVariables(builder);
